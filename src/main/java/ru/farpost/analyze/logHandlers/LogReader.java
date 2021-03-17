b23a4ae7ac615096f926a3abec9b47eccccd7fd2
KirillAmber <sweetserialkiller2000100@gmail.com>
2021-03-17 11:16:36 +1000

    add alternative reading file

--- src/main/java/ru/farpost/analyze/logHandlers/LogReader.java
+++ src/main/java/ru/farpost/analyze/logHandlers/LogReader.java
@@ -2,9 +2,8 @@ package ru.farpost.analyze.logHandlers;
 
 import ru.farpost.analyze.models.InputQueueSingleton;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 public class LogReader extends Thread {
 
@@ -16,8 +15,27 @@ public class LogReader extends Thread {
     public void run() {
         super.run();
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            if(!input.ready()){
+                System.out.println("¬ведите название файла (‘айл должен быть в том же каталоге," +
+                        " что и .jar:");
+                Scanner in = new Scanner(System.in);
+                String fileName = in.next();
+                File logFile = new File(fileName);
+                System.out.println(logFile.getAbsolutePath());
+
+                input = new BufferedReader(new FileReader(logFile));
+            }
+            read(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void read(BufferedReader input) {
         String stringLog = null;
-        while(true) {
+        while (true) {
             try {
                 if ((stringLog = input.readLine()) == null) break;
             } catch (IOException e) {
